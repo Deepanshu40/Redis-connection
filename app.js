25f7ec1bf5d233b2671e2cf3a963c94f08655e75
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-07 09:28:10 +0530

    restructuring app.js

--- app.js
+++ app.js
@@ -1,42 +1,54 @@
 const express = require('express');
-const app = express();
-const port = 8080;
 const Redis = require('ioredis');
-const redis = new Redis('rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR@oregon-redis.render.com:6379');
-
-
 const Queue = require('bull');
 
-// const panQueue = new Queue('panQueue', {
-//         redis: {
-//         url: 'rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR@oregon-redis.render.com:6379' 
-//         }
-//     });
+const app = express();
+const port = 8080;  // Make sure this matches the log statement
 
-const panQueue = new Queue('panQueue', {
+// Configuration for Redis connection
+const redisUrl = 'rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR@oregon-redis.render.com:6379';
+const redis = new Redis(redisUrl);
+const redisOptions = {
   redis: {
-    port: 6379, // Redis port
-    host: 'oregon-redis.render.com', // Redis host
-    password: 'F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR', // Redis password
-    tls: {} // This might be necessary if you're using 'rediss://'
+    url: redisUrl,
+    tls: {}  // Necessary if using 'rediss://' to indicate SSL/TLS connection
+  }
+};
+
+// Create a new Bull queue with the specified Redis configuration
+const panQueue = new Queue('panQueue', redisOptions);
+
+// Redis connection success
+redis.on('connect', () => {
+  console.log('Connected to Redis:', redis.options);
+});
+
+// Redis connection error logging
+redis.on('error', (err) => {
+  console.error('Redis connection error:', err);
+});
+
+// Bull queue error logging
+panQueue.on('error', (err) => {
+  console.error('Bull queue error:', err);
+});
+
+// Simple test to verify Redis connection
+redis.set('test_key', 'test_value', (err) => {
+  if (err) {
+    console.error('Error setting key on Redis:', err);
+  } else {
+    redis.get('test_key', (err, result) => {
+      if (err) {
+        console.error('Error getting key from Redis:', err);
+      } else {
+        console.log('Value retrieved from Redis:', result); // Should log 'test_value'
+      }
+    });
   }
 });
 
-
-redis.on('connect', () => {
-        console.log(redis.options);
-});
-
-
-redis.on('error', (err) => {
-    console.error('Redis connection error:', err);
-});
-
-
-panQueue.on('error', (err) => {
-    console.error('Bull queue error:', err);
-});
-
+// Start Express server
 app.listen(port, () => {
-    console.log('Listening on port 8000');
+  console.log(`Listening on port ${port}`);
 });
