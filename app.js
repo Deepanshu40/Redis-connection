372413272c512269be535ba6beed90ae301bccdc
Deepanshu40 <144712703+Deepanshu40@users.noreply.github.com>
2024-05-07 09:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -7,11 +7,21 @@ const redis = new Redis('rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJ
 
 const Queue = require('bull');
 
+// const panQueue = new Queue('panQueue', {
+//         redis: {
+//         url: 'rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR@oregon-redis.render.com:6379' 
+//         }
+//     });
+
 const panQueue = new Queue('panQueue', {
-        redis: {
-        url: 'rediss://red-cosclhq0si5c739t8bpg:F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR@oregon-redis.render.com:6379' 
-        }
-    });
+  redis: {
+    port: 6379, // Redis port
+    host: 'oregon-redis.render.com', // Redis host
+    password: 'F9IpuAyVAw9n6DYGFWrjJmiJeZWqXKXR', // Redis password
+    tls: {} // This might be necessary if you're using 'rediss://'
+  }
+});
+
 
 redis.on('connect', () => {
         console.log(redis.options);
